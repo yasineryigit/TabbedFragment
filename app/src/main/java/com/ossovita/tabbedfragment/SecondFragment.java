3aeb01becfd14db4331006770c1391bbdc645c9f
sancaryum <sancaryum@gmail.com>
2020-12-26 19:14:47 +0300

    First commit

---
+++ app/src/main/java/com/ossovita/tabbedfragment/SecondFragment.java
@@ -0,0 +1,52 @@
+package com.ossovita.tabbedfragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ossovita.tabbedfragment.ui.main.PageViewModel;
+
+import org.w3c.dom.Text;
+
+public class SecondFragment extends Fragment {
+
+    PageViewModel pageViewModel;
+    //her yerden ulaşılabilmesi için static
+    public static SecondFragment newInstance(){
+        return new SecondFragment();
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new  ViewModelProvider(requireActivity()).get(PageViewModel.class);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.textView);
+        pageViewModel.getName().observe(requireActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
+    }
+}
